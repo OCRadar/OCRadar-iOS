eca3e522311a09c1ca61c0ca71dc6fd38d50b8fd
ankthba <anikethdb1@gmail.com>
2024-06-04 21:12:50 -0400

    Initial Commit

---
+++ OCR App iOS/OCR_App_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCR_App_iOSApp.swift
+//  OCR App iOS
+//
+//  Created by Aniketh Bandlamudi on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OCR_App_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

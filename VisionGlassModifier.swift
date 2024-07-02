eead7662746105b57812170e179e12d225c7f282
ankthba <anikethdb1@gmail.com>
2024-07-01 23:19:29 -0400

    disabling vision glass

--- VisionGlassModifier.swift
+++ VisionGlassModifier.swift
@@ -1,38 +1,38 @@
+////
+////  VisionGlassModifier.swift
+////  OCR App iOS
+////
+////  Created by Aniketh Bandlamudi on 6/7/24.
+////
 //
-//  VisionGlassModifier.swift
-//  OCR App iOS
+//import Foundation
+//import SwiftUI
 //
-//  Created by Aniketh Bandlamudi on 6/7/24.
+//struct VisionGlassModifier: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//            .padding()
+//            .background(BlurView(style: .systemMaterial))
+//            .cornerRadius(10)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 10)
+//                    .stroke(
+//                        LinearGradient(gradient: Gradient(stops: [
+//                            .init(color: Color.white.opacity(0.1), location: 0),
+//                            .init(color: Color.white.opacity(0.15), location: 0.15),
+//                            .init(color: Color.white.opacity(1), location: 0.5),
+//                            .init(color: Color.white.opacity(0.15), location: 0.85),
+//                            .init(color: Color.white.opacity(0.1), location: 1)
+//                        ]), startPoint: .topLeading, endPoint: .bottomTrailing),
+//                        lineWidth: 1
+//                    )
+//            )
+//            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
+//    }
+//}
 //
-
-import Foundation
-import SwiftUI
-
-struct VisionGlassModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding()
-            .background(BlurView(style: .systemMaterial))
-            .cornerRadius(10)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(
-                        LinearGradient(gradient: Gradient(stops: [
-                            .init(color: Color.white.opacity(0.1), location: 0),
-                            .init(color: Color.white.opacity(0.15), location: 0.15),
-                            .init(color: Color.white.opacity(1), location: 0.5),
-                            .init(color: Color.white.opacity(0.15), location: 0.85),
-                            .init(color: Color.white.opacity(0.1), location: 1)
-                        ]), startPoint: .topLeading, endPoint: .bottomTrailing),
-                        lineWidth: 1
-                    )
-            )
-            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 5)
-    }
-}
-
-extension View {
-    func visionGlassEffect() -> some View {
-        self.modifier(VisionGlassModifier())
-    }
-}
+//extension View {
+//    func visionGlassEffect() -> some View {
+//        self.modifier(VisionGlassModifier())
+//    }
+//}
